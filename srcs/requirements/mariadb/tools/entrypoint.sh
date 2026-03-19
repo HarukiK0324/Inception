7814f87bfb76b9beb09af4174f71aad60a535dc4
haruki <71203445+HarukiK0324@users.noreply.github.com>
2026-03-19 15:40:49 +0900

    fixed secrets path

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+DB_PASS=$(cat /run/secrets/db_password)
+DB_ROOT_PASS=$(cat /run/secrets/db_root_password)
+
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
     cat << EOF > /tmp/init.sql
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '../secrets/db_root_password.txt';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '../secrets/db_password.txt';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,8 +2,12 @@
 
 cd /var/www/html
 
+DB_PASS=$(cat /run/secrets/db_password)
+WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASS=$(cat /run/secrets/wp_user_password)
+
 echo "Waiting for MariaDB to start..."
-while ! mariadb -h mariadb -u "$MYSQL_USER" -p"../secrets/db_password.txt" -e "SELECT 1;" >/dev/null 2>&1; do
+while ! mariadb -h mariadb -u "$MYSQL_USER" -p"$DB_PASS" -e "SELECT 1;" >/dev/null 2>&1; do
     sleep 3
 done
 echo "MariaDB is up and running!"
@@ -15,20 +19,20 @@ if [ ! -f wp-config.php ]; then
 
     wp config create --dbname="$MYSQL_DATABASE" \
                      --dbuser="$MYSQL_USER" \
-                     --dbpass="../secrets/db_password.txt" \
+                     --dbpass="$DB_PASS" \
                      --dbhost=mariadb:3306 \
                      --allow-root
 
     wp core install --url="$DOMAIN_NAME" \
                     --title="Inception" \
                     --admin_user="$WP_ADMIN_USER" \
-                    --admin_password="../secrets/wp_admin_password.txt" \
+                    --admin_password="$WP_ADMIN_PASS" \
                     --admin_email="$WP_ADMIN_EMAIL" \
                     --allow-root
     
     wp user create "$WP_USER" "$WP_USER_EMAIL" \
                    --role=author \
-                   --user_pass="../secrets/wp_user_password.txt" \
+                   --user_pass="$WP_USER_PASS" \
                    --allow-root
     
     chown -R www-data:www-data /var/www/html
